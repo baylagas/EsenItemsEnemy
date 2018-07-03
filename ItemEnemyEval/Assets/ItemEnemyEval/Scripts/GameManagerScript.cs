6bb173964b46105d39201107ed01568d7b999cd6
DESKTOP-VOC1MRF\balbi <balbino_a@hotmail.com>
2018-07-03 14:07:28 -0600

    added ui elements
    added ui elements like player health and win text. also a reset level when you win.

--- ItemEnemyEval/Assets/ItemEnemyEval/Scripts/GameManagerScript.cs
+++ ItemEnemyEval/Assets/ItemEnemyEval/Scripts/GameManagerScript.cs
@@ -1,16 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
     public ItemLinkerScript linkerScript;
+    public Text winText;
+    public bool oneTime;
+
+    private void Start()
+    {
+        SetWinText("");
+    }
 
     private void Update()
     {
-        if(linkerScript.GotLinkedItem)
+        if(linkerScript.GotLinkedItem && oneTime==false)
         {
             Debug.Log("You Win!!");
+            SetWinText("You Win!!");
+            StartCoroutine("ResetLevel");
+            oneTime = true;
         }
+    }
+
+    public IEnumerator ResetLevel()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Main");
+    }
+
+    public void SetWinText(string message)
+    {
+        winText.text = message;
     }
 }
--- ItemEnemyEval/Assets/ItemEnemyEval/Scripts/PlayerHealth.cs
+++ ItemEnemyEval/Assets/ItemEnemyEval/Scripts/PlayerHealth.cs
@@ -1,14 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth;
     public int currentHealth;
+    public Text playerHealthText;
 
     private void Start()
     {
         currentHealth = maxHealth;
+        SetPlayerHealthText();
+    }
+
+    private void SetPlayerHealthText()
+    {
+        playerHealthText.text = "PHealth: " + currentHealth;
     }
 
     public void AddHealth(int value)
@@ -18,6 +27,7 @@ public class PlayerHealth : MonoBehaviour
         {
             currentHealth = maxHealth;
         }
+        SetPlayerHealthText();
     }
 
     public void RemoveHealth(int value)
@@ -28,6 +38,7 @@ public class PlayerHealth : MonoBehaviour
             //currentHealth = 0;
             RestartLevel();
         }
+        SetPlayerHealthText();
     }
 
     private void RestartLevel()
